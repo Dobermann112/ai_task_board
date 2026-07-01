d5c32cf4a529791c0400e05f687d48ac4ce162db
dobermann112 <ruffy112@icloud.com>
2026-07-01 21:39:40 +0900

    prismaとSQLiteを導入

---
+++ prisma/migrations/20260701123232_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'TODO',
+    "priority" TEXT NOT NULL DEFAULT 'MEDIUM',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
